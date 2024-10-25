02ae2034fd27a45631364bbd67b6cd833e262540
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-25 18:28:37 +0530

    ✨ feat: GET_USER route - /user/me

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -16,7 +16,18 @@ userRouter.post("/create/:userId", async (req, res) => {
   res.status(parsedResponse?.statusCode).json(parsedResponse)
 });
 
-userRouter.get("/", (req,res) => {
-  process.exit(1)
+userRouter.get("/me", async (req, res) => {
+  const { userId } = req.body
+  if (!userId) {
+    res.status(400).json({ statusMessage: "userId is not present in body" })
+  }
+  const response = await RedisManager.getInstance().sendAndAwait({
+    type: REQUEST_TYPE.GET_USER,
+    data: {
+      userId,
+    },
+  });
+  const parsedResponse = JSON.parse(response?.payload?.message)
+  res.status(parsedResponse?.statusCode).json(parsedResponse)
 })
 
