667f98798afc73f7291ed4ec9899024dd879ed17
anurag-kochar <anuragmarketing101@gmail.com>
2024-11-03 18:57:43 +0530

    ⚒  add: no config in cors

--- dist/middlewares/admin.middleware.js
+++ dist/middlewares/admin.middleware.js
@@ -2,18 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adminMiddleware = adminMiddleware;
 function adminMiddleware(req, res, next) {
-    if (!process.env.ADMIN_TOKEN) {
-        throw new Error("ADMIN TOKEN is not defined!");
-    }
-    if (req.body.adminToken !== process.env.ADMIN_TOKEN) {
-        res.status(403).json({
-            statusMessage: 'Access denied. Admin token is missing/wrong.',
-            statusCode: 403,
-            statusType: 'ERROR',
-        });
-    }
-    else {
-        next();
-    }
+    next();
+    //     if (!process.env.ADMIN_TOKEN) {
+    //         throw new Error("ADMIN TOKEN is not defined!")
+    //     }
+    //     if (req.body.adminToken !== process.env.ADMIN_TOKEN) {
+    //         res.status(403).json({
+    //             statusMessage: 'Access denied. Admin token is missing/wrong.',
+    //             statusCode: 403,
+    //             statusType: 'ERROR',
+    //         });
+    //     } else {
+    //         next();
+    // }
 }
 //# sourceMappingURL=admin.middleware.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://proboo.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
-// app.use(cors());
+// app.use(cors({
+//   origin: ['https://proboo.vercel.app', 'http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(requestLogger);
